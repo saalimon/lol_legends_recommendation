1cbe29eeb3c994b2b285c66802bb816004fe2ddb
bx-NT <bx-nt@bx.kmutt.ac.th>
2022-02-08 18:44:04 +0700

    initial project skeleton

---
+++ src/containers/App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import '../css/pico.min.css';
+import Champions from './Champions';
+function App() {
+  return (
+    <div className="App">
+      <header  className="container">
+            <hgroup>
+                <h1>LOL, Draft Pick Recomendation System!</h1>
+                <h2>League of Legends is a team-based game with over 140 champions to make epic plays with.</h2>
+              </hgroup>
+        </header >
+        <main className="container">
+        <div className="grid">
+                <div className="grid">
+                    {[...Array(5)].map((x, i) =>
+                      <div><img src="https://ddragon.leagueoflegends.com/cdn/img/champion/loading/Aatrox_0.jpg" alt="Aatrox"/><span class="name">Aatrox</span></div>
+                    )}  
+                </div>
+                <div className="grid">
+                    {[...Array(5)].map((x, i) =>
+                      <div><img src="https://ddragon.leagueoflegends.com/cdn/img/champion/loading/Aatrox_0.jpg" alt="Aatrox"/><span class="name">Aatrox</span></div>
+                    )}  
+                </div>
+          </div>
+          <div className="grid">
+                <div className="grid">
+                    {[...Array(5)].map((x, i) =>
+                      <div><img src="https://ddragon.leagueoflegends.com/cdn/img/champion/loading/Aatrox_0.jpg" alt="Aatrox"/><span class="name">Aatrox</span></div>
+                    )}  
+                </div>
+                <div className="grid">
+                    {[...Array(5)].map((x, i) =>
+                      <div><img src="https://ddragon.leagueoflegends.com/cdn/img/champion/loading/Aatrox_0.jpg" alt="Aatrox"/><span class="name">Aatrox</span></div>
+                    )}  
+                </div>
+          </div>
+          <Champions />
+        </main>
+        <footer className="container">
+            <small>Built with <a href="https://picocss.com">Pico</a> ✖ <a href="https://github.com/saalimon">Saalimon</a></small>
+        </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/containers/Champions.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+function Champions() {
+    const [champion, setData] = useState([]);
+    useEffect(() => {
+        fetch('http://ddragon.leagueoflegends.com/cdn/12.3.1/data/en_US/champion.json')
+        .then(response => {
+            if(response.ok){
+                return response.clone().json()
+            }
+            throw response
+        }).then(data => {
+            setData(data['data'])
+        }).catch(error => {
+            console.error("Error fetching data", error)
+        })
+    }, [])
+    if(!champion){
+        return null;
+    }
+    return(
+        <div>
+
+            {Object.keys(champion).map(key => {
+                let url = 'https://ddragon.leagueoflegends.com/cdn/12.3.1/img/sprite/'+champion[key]['image']['sprite']
+                let x  = champion[key]['image']['x']
+                let y =  champion[key]['image']['y']
+                console.log(url)
+                return (<div className="champion" style={{
+                                                    width: '75px',
+                                                    height: '75px',
+                                                    textAlign: 'center',
+                                                    textTransform: 'uppercase',
+                                                    fontSize: '9px',
+                                                    display: 'inline-block'}}>
+                            <div className="champion-icon" style={{
+                                                            margin: '0 auto',
+                                                            backgroundImage: `url(${url})` ,
+                                                            width: '48px',
+                                                            height: '48px',
+                                                            backgroundPosition: `-${x}px ${y}px`,
+                                                            }}>
+                            </div>
+                            {key}
+                    </div>)   
+                })
+            }     
+        </div>
+    )
+}
+export default Champions
